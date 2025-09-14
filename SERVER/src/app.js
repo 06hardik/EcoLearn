3a1e2ca1c912cf47070edaa672f2ceb3498dbce4
Hardik <jindalhardik06@gmail.com>
2025-09-14 16:57:02 +0530

    some more changes

--- SERVER/src/app.js
+++ SERVER/src/app.js
@@ -1,24 +1,33 @@
-import express, { urlencoded } from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-let corsOptions = {
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}
-app.use(cors(corsOptions))
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 
-//routes import
-import userRouter from './routes/user.routes.js'
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-//routes declaration
-app.use("/api/users",userRouter) // middleware
+// routes import
+import userRouter from './routes/userRoutes.js';
+import courseRouter from './routes/courseRoutes.js';
+import eventRouter from './routes/eventRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
+import submissionRouter from './routes/submissionRoutes.js';
 
+// routes declare
+app.use("/api/users", userRouter);
+app.use("/api/courses", courseRouter);
+app.use("/api/events", eventRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/submissions", submissionRouter);
 
-export {app}
+export { app };
--- SERVER/src/index.js
+++ SERVER/src/index.js
@@ -1,12 +1,21 @@
 import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-connectDB().then(()=>{
-    app.on("error",()=>{console.log("ERR: ",error)})
-    app.listen(process.env.PORT||8000,()=>{console.log(`Server is running at ${process.env.PORT}`)})
-}).catch((error)=>{
-    console.log("MONGO DB connection failed ERR: ",error)
-})
+dotenv.config({
+    path: './.env' // Ensure the path to your .env file is correct
+});
+
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("SERVER ERROR: ", error);
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`🚀 Server is running at port: ${process.env.PORT || 8000}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGO DB connection failed !!! ", err);
+    });
