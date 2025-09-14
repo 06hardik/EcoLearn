cc65330f80c2099c2bf2ca411f216569e65f3da9
Hardik <jindalhardik06@gmail.com>
2025-09-15 01:50:15 +0530

    some changes

--- SERVER/src/controllers/user.controller.js
+++ SERVER/src/controllers/user.controller.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js"; 
 
 const registerUser = asyncHandler(async (req, res) => {
-    console.log('hi')
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
@@ -35,38 +34,44 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
+    console.log('hi')
     const user = await User.findById(req.user.id).select("-password");
+    console.log("hi11")
     res.status(200).json(user);
 });
 
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-
     if (user && (await bcrypt.compare(password, user.password))) {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '30d',
+            expiresIn: '10d',
         });
 
-        res.cookie('token', token, {
-            httpOnly: true, 
-            secure: process.env.NODE_ENV === 'production', 
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
-        });
+        const options = {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'lax',
+            maxAge: 30 * 24 * 60 * 60 * 1000 
+        };
 
-        res.status(200).json({
-            message: "Login successful",
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-            },
-        });
+        return res
+            .status(200)
+            .cookie('token', token, options)
+            .json({
+                message: "Login successful",
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                },
+            });
+
     } else {
-        res.status(401).json({ message: "Invalid email or password" });
+        return res.status(401).json({ message: "Invalid email or password" });
     }
 });
+
 
 const logoutUser = asyncHandler(async (req, res) => {
     res.clearCookie('token');
