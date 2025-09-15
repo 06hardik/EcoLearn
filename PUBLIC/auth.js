deeceae612936043f72d177bb128f6d007a5c470
06hardik <hardikjindal2020@gmail.com>
2025-09-16 03:43:32 +0530

    Update auth.js

--- PUBLIC/auth.js
+++ PUBLIC/auth.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const handleLogout = async () => {
         await fetch(`${API_BASE_URL}/users/logout`, { method: 'POST', credentials: 'include' });
-        currentUser = null;
+        window.currentUser = null;
         window.location.href = '/index.html';
     };
 
